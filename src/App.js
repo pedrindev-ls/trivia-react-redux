f94d0a5c0425df2a6ad4dcfa87a275021af364dc
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-04-06 16:13:58 -0300

    :heavy_check_mark: feat: requisito 3 feito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login/Login';
+import Settings from './Pages/Settings/Settings';
 import './App.css';
 
 export default function App() {
   return (
     <Switch>
-      <Route exact patch="/" render={ (props) => <Login { ...props } /> } />
+      <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+      <Route exact path="/settings" render={ (props) => <Settings { ...props } /> } />
     </Switch>
   );
 }
