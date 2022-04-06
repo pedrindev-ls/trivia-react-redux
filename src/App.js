a713fa2f33d8e1255f7ed5f8aa66dc4bb659cd62
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-04-06 19:13:55 -0300

    :heavy_check_mark: feat: requisito 5

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login/Login';
 import './App.css';
+import Game from './Pages/Game/Game';
 
 export default function App() {
   return (
     <Switch>
-      <Route exact patch="/" render={ (props) => <Login { ...props } /> } />
+      <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+      <Route exact path="/game" render={ (props) => <Game { ...props } /> } />
     </Switch>
   );
 }
--- src/Pages/Game/Game.js
+++ src/Pages/Game/Game.js
@@ -1,10 +1,96 @@
 import React from 'react';
+import { apiGetQuestions, apiGetToken } from '../../Services/api';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+      questions: [],
+      currentQuestion: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.getQuestions();
+  }
+
+  getQuestions = () => {
+    const { token } = this.state;
+    apiGetQuestions(token)
+      .then(async (questions) => {
+        const responseCodeError = 3;
+        if (questions.response_code === responseCodeError) {
+          const receivedToken = await apiGetToken();
+          const receivedQuestions = await apiGetQuestions(receivedToken.token);
+          this.setState({
+            token: receivedToken.token,
+            questions: receivedQuestions.results,
+          });
+        } else {
+          this.setState({ questions: questions.results });
+        }
+      })
+      .catch((error) => { console.log(error); });
+  }
+
+  renderQuestion = () => {
+    const { questions, currentQuestion } = this.state;
+    if (questions.length > 0) {
+      const question = questions[currentQuestion];
+      return (
+        <section>
+          <h2 data-testid="question-category">{question.category}</h2>
+          <p data-testid="question-text">{question.question}</p>
+        </section>
+      );
+    }
+  }
+
+  generateAnswers = (question) => {
+    const answers = question.incorrect_answers
+      .map((incorrectAnswer, index) => ((
+        <button
+          type="button"
+          data-testid={ `wrong-answer-${index}` }
+          key={ index }
+        >
+          {incorrectAnswer}
+        </button>
+      )));
+    answers.push((
+      <button
+        type="button"
+        data-testid="correct-answer"
+        key={ answers.length }
+      >
+        {question.correct_answer}
+      </button>
+    ));
+    answers.sort();
+    return answers;
+  }
+
+  renderAnswers = () => {
+    const { questions, currentQuestion } = this.state;
+    if (questions.length > 0) {
+      const answers = this.generateAnswers(questions[currentQuestion]);
+      return (
+        <section>{answers}</section>
+      );
+    }
+  }
+
   render() {
     // const { name, email, play } = this.state;
     return (
-      <div> Game </div>
+      <main>
+        <h1> Game </h1>
+        <div data-testid="answer-options">
+          {this.renderQuestion()}
+          {this.renderAnswers()}
+        </div>
+      </main>
     );
   }
 }
