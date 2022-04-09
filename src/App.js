d1ad7c0d2c30ae54ac37a8ef1e41c6f37efebc9c
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-04-09 17:04:08 -0300

    :heavy_check_mark: feat: Requisito 11 feito

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login/Login';
 import Settings from './Pages/Settings/Settings';
+import Feedback from './Pages/Feedback/Feedback';
 import Game from './Pages/Game/Game';
 import './App.css';
 
@@ -11,6 +12,7 @@ export default function App() {
       <Route exact path="/" render={ (props) => <Login { ...props } /> } />
       <Route exact path="/game" render={ (props) => <Game { ...props } /> } />
       <Route exact path="/settings" render={ (props) => <Settings { ...props } /> } />
+      <Route exact path="/feedback" render={ (props) => <Feedback { ...props } /> } />
     </Switch>
   );
 }
